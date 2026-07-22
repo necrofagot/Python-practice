4e85b57a13d75bc845d2bfadaf0f15811cc2ea4f
Ignacio Zanzuchi <nachozanzuch@gmail.com>
2026-07-22 14:49:23 -0300

    Update funciones
    Corrección de funciones:
    -Generación de lista random ya no es una función.
    -Función listado_checking ahora toma un index de parámetro para retornar el elemento en ese index de la lista.
    -Función palindromo ahora usa métodos .lower y .replace para evitar errores de espaciado.
    Funciones nuevas:
    -Función factorial: toma una lista de parámetro y devuelve una nueva con los factoriales de cada elemento de la primer lista.
    -Función múlti: toma una lista y un numero para devolver una lista con los números que solamente sean múltiplos del segundo parámetro tomado.

--- listas/listas-y-funciones.py
+++ listas/listas-y-funciones.py
@@ -4,10 +4,10 @@ lista_example = []
 n = int(input('Ingrese cantidad de numeros a generar'))
 
 #Generar lista random
-def lista_random(lista):
-    for _ in range(n):
-        numero = random.randint(1,200)
-        lista.append(numero)
+for _ in range(n):
+    numero = random.randint(1,200)
+    lista_example.append(numero)
+
 
 #Generar el numero aureo en una lista de N numeros con una funcion:
 def fibonacci(lista):
@@ -18,16 +18,31 @@ def fibonacci(lista):
         lista.append(lista[-1] + lista[-2])
     return lista
 
+#imprimir los multiplos de un numero x:
+def multi(lista,x):
+    multi = []
+    for num in lista:
+        if num % x == 0:
+            multi.append(num)
+    return multi
+
 #Hacer una funcion que diga cual es el tercer elemento, me diga la cantidad de elementos y si la lista esta vacia:
-def listado_checking(lista):
+def listado_checking(lista, index):
     if len(lista) == 0:
         return 'Su lista esta vacia'
-    return f'El tercer elemento de su lista es:{lista[2]} y su lista contiene {len(lista)} elementos'
+    return f'El {index} elemento de su lista es:{lista[index]} y su lista contiene {len(lista)} elementos'
 
 #Funcion que reconozca palindromos:
 def palindromo(str):
-    for i in range(len(str) // 2):
-        if str[i] != str[-(i + 1)]:
-            return False
-    return True     
-#No funciona! tengo que borrar los espacios en blanco!
+    strpal = str.lower().replace(' ','')
+    return strpal == strpal[::-1]
+
+#Funcion para sacar factorial
+def factorial(lista):
+    lista_f = []
+    for num in lista:
+        aux = num
+        for i in range (1,num):
+            aux = aux * (num - i)
+        lista_f.append(aux)
+    return lista_f
